72e0ce3b71c9e1bdebf07e6eaa24fad8bbe4a065
choonkeat <choonkeat@gmail.com>
2012-03-06 12:46:55 +0800

    minor style updates

--- bin/generate.bash
+++ bin/generate.bash
@@ -5,8 +5,9 @@ echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www
     <link href="gritnet.css" media="screen" rel="stylesheet" type="text/css" />
   </head>
   <body>
-    <h1><sup>git</sup><sub>hub</sub><sup>net</sup><sub>work</sub></h1>
+    <h1>branchesapp</h2>
+    <h2><sup>visual</sup><sub>izing</sub><sup>git</sup><sub>hub</sub><sup>net</sup><sub>work</sub></h2>
 ' > index.html
 for x in */*/index.html; do p=`dirname $x`; echo "<li><a href='${p}/'>${p}</a></li>"; done >> index.html
-echo "<p><a href='mailto:contactme@choonkeat.com'>contact me</a></p></body></html>" >> index.html
+echo "<p><a href='mailto:contactme@choonkeat.com'>contact me</a> | <a  href='https://github.com/choonkeat/branchesapp'>source code</a></p></body></html>" >> index.html
 cd -
